fce45ad8a8b187b84e0a44631fd64e06d89de19e
Parthiv <94118901+deyparthiv@users.noreply.github.com>
2024-10-25 22:54:54 +0100

    state storage in smart contract working

--- BackTheBlock/contracts/voting/src/lib.rs
+++ BackTheBlock/contracts/voting/src/lib.rs
@@ -9,23 +9,29 @@ use soroban_sdk::{contract, contracttype, contractimpl, vec, Env, String, Vec,lo
 //LIST TO STORE REGISTERED MEMBERS
 #[derive(Clone)]
 #[contracttype]
-pub enum MEMBERS{
+pub enum Members {
     Vec,
 }
-
 #[contract]
 pub struct VotingContract;
 
 #[contractimpl]
 impl VotingContract {
+    pub fn setup(env: Env) -> (){
+        env.storage().persistent().set(&Members::Vec, &Vec::<String>::new(&env));
+        log!(&env,"after setup:", env.storage().persistent().get(&Members::Vec).unwrap_or(vec![&env,String::from_str(&env,"failure")]));
+    }
     pub fn addMember(env: Env, member: String) -> Vec<String> {
         //load list of members from env into function for mutation
-        let mut members: Vec<String> = env.storage().instance().get(&MEMBERS::Vec).unwrap_or(vec![&env, String::from_str(&env,"failure to fetch list")]);
+        let mut members: Vec<String> = env.storage()
+                                        .persistent()
+                                        .get(&Members::Vec)
+                                        .unwrap_or(vec![&env, String::from_str(&env,"failure to fetch list")]);
         log!(&env, "members before adding new member: {}",members);
         members.push_back(member);
         log!(&env, "members: {}",members);
-        env.storage().instance().set(&MEMBERS::Vec, &members);
-        env.storage().instance().extend_ttl(100, 100);
+        env.storage().persistent().set(&Members::Vec, &members);
+        //env.storage().instance().extend_ttl(100, 100);
         members
     }
 }
--- BackTheBlock/contracts/voting/src/test.rs
+++ BackTheBlock/contracts/voting/src/test.rs
@@ -4,13 +4,23 @@ use super::*;
 use soroban_sdk::{vec, Env, String};
 
 #[test]
-fn test() {
+fn test1() {
     let env = Env::default();
     let contract_id = env.register_contract(None, VotingContract);
     let client = VotingContractClient::new(&env, &contract_id);
+    client.setup();
     assert_eq!(
        client.addMember(&String::from_str(&env,"James")), vec![&env,String::from_str(&env,"James"),]
     );
+}
+
+#[test]
+fn test2() {
+    let env = Env::default();
+    let contract_id = env.register_contract(None, VotingContract);
+    let client = VotingContractClient::new(&env, &contract_id);
+    client.setup();
+    client.addMember(&String::from_str(&env,"James"));
     assert_eq!(
         client.addMember(&String::from_str(&env,"Parthiv")),vec![&env,String::from_str(&env,"James"),String::from_str(&env,"Parthiv")]
     );
